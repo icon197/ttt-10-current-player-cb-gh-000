620266bceaac8c5f93e6cfc9972b0e9d9dadc98d
Phan Huynh Tan <tanb1509892@student.ctu.edu.vn>
2019-01-07 04:59:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,3 +12,6 @@ def current_player(board)
   puts current_player(board)
   return (current_player(board) % 2 == 0) ? "X" : "O"
 end
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+puts turn_count(board)
